034bcbcf16e73eda38d9bdac37d0290da188e6b4
Gaxel18 <alejandro_cs_09@hotmail.com>
2023-09-28 19:45:29 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/DAWII_CL1_Grupo9/DawiiCl1Grupo9Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL1_Grupo9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl1Grupo9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl1Grupo9Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWII_CL1_Grupo9/DawiiCl1Grupo9ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL1_Grupo9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl1Grupo9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
